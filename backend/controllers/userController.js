e4e4610071b32baf27538dfe67500bb523d4f636
mujammil28 <64261184+mujammil28@users.noreply.github.com>
2025-05-02 10:58:27 +0530

    mutual frinds logic added

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,21 +1,23 @@
-import axios from 'axios';
+import axios from "axios";
+export const getUserByUsername = async (req, res) => {
+  const { username } = req.params;
 
-const getGithubUser=async(req,res)=>{
+  try {
+    const response = await axios.get(`https://api.github.com/users/${username}`);
+    const data = response.data;
 
-        try{
-            const {userName}=req.params;
-            const user = await axios.get(`https://api.github.com/users/${username}`);
-            const repo= await axios.get(`https://api.github.com/users/${username}/repo`);
+    const user = {
+      username: data.login,
+      name: data.name,
+      avatar_url: data.avatar_url,
+      bio: data.bio,
+      location: data.location,
+      html_url: data.html_url
+    };
 
-            res.json({
-                    user:user.data,
-                    repo:user.repo,
-            })
-
-        }catch(err){
-            res.status(404).json({err:'user not found'});
-
-        }
-}
-
-export default getGithubUser;
+    res.json(user);
+  } catch (error) {
+    console.error('GitHub fetch error:', error.message);
+    res.status(404).json({ message: 'GitHub user not found' });
+  }
+};
