cea3202c6bec8b4b00dba1a112335363a91055f9
mujammil28 <64261184+mujammil28@users.noreply.github.com>
2025-05-02 11:16:00 +0530

    database and server running successfully

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,9 @@
 import express from 'express';
 import cors from 'cors';
-import userRoutes from './routes/user.js';
+import router from './routes/user.js';
 
 const app=express();
 app.use(cors());
-app.use('app/users',userRoutes);
+app.use('/api/users',router);
 
 export default app;
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
+import express from 'express';
+import cors from 'cors';
+const app = express();
+import router from './routes/user.js';
+app.use(cors());
+app.use('/api', router);
 
-import app from './app';
-const port=5050;
-
-app.listen(port,()=>{
-    console.log("Server is running on port 5050");
-})
+const PORT = 5000;
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
